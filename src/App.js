e062227b449e3ad9b5e01cea6a91e9a59dd783a2
olanetsoft <heedris2olubisi@gmail.com>
2020-02-11 18:16:22 +0100

    APP UPDATED

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <h1>Welcome to REACT! :)</h1>
+            <h1>This is a React application created without using create-react app :)</h1>
         )
     }
 }
