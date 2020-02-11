56d231c37d3ff52696c93723a73eca23e12cd918
olanetsoft <heedris2olubisi@gmail.com>
2020-02-11 17:33:55 +0100

    .BABELRC FILE CREATED TELLING BABEL WHAT TO USE WHEN CONVERTING ES6 TO ES5

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/App.js',
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     },
     devServer: {
         contentBase: path.join(__dirname, 'public')
