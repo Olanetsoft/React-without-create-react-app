2f5b19f029d9cdebb97c1c02237429d7b70aca45
olanetsoft <heedris2olubisi@gmail.com>
2020-02-11 18:00:56 +0100

    COMMENTS ADDED

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     render() {
         return (
-            <h1>Welcome to REACT!</h1>
+            <h1>Welcome to REACT! :)</h1>
         )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
     },
+    // Here we are telling babel-loader to look for only .js files to convert to ES5 code but exclude the .js files from node_modules directory
     module: {
         rules: [{
             loader: 'babel-loader',
@@ -12,7 +13,7 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    mode: 'development',
+    mode: 'development',//This is added to inform webpack, that it should run in development mode (default is production mode) 
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
